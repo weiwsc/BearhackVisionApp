81d9fd8e8b422d2effee8924181fb56d90eca628
Wangsicong Wei <weiwa@sheridancollege.ca>
2026-04-25 11:37:36 -0400

    Initial Commit

---
+++ VisionAppTests/VisionAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  VisionAppTests.swift
+//  VisionAppTests
+//
+//  Created by Dvillera Vian on 2026-04-25.
+//
+
+import Testing
+@testable import VisionApp
+
+struct VisionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
